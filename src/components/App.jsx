3c56856d42ea6c05db838cdba345c68c0532318e
Oakmoater <dubrovecva@gmail.com>
2023-05-21 10:03:22 +0200

    fixed bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Searchbar from "./Searchbar/Searchbar";
 import { getImages } from "API/getSearchImages";
 import ImageGallery from "./ImageGallery/ImageGallery";
